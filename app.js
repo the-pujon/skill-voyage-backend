d9cca0c7f7e568cf1427c1b744c0104841b347e1
Pujon Das Auvi <auvipujondas@gmail.com>
2023-10-17 11:52:31 +0600

    users endpoints added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("./src/config/db")
+const userRouter = require("./src/routes/user.routes");
 
 
 
@@ -9,10 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/api/users", userRouter);
 
 //Home page
 app.get("/", (req, res) => {
-  res.status(200).sendFile(__dirname + "/./view/index.html");
+  res.status(200).sendFile(__dirname + "/./src/view/index.html");
 });
 
 
