f0008ea71d5a62ca772c28fd8602cc03e1798f63
Pujon Das Auvi <auvipujondas@gmail.com>
2023-10-17 11:44:24 +0600

    all files added

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const config = require("./src/config/config");
 const port = config.app.port;
 
 app.listen(port, async (req, res) => {
-  console.log(`Your server is running in http://localhost:${port}`);
+  console.log(`Your server is running in http://localhost:${port}`)
 });
