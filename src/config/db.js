9569aaf04cd82f55cbef325e29f13ec591f1e1bd
Pujon Das Auvi <auvipujondas@gmail.com>
2023-10-17 11:45:19 +0600

    database connected

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const config = require("./config");
 
-const db_url = config.db.dbURL;
 
 mongoose
-  .connect(db_url)
+  .connect(config.db.dbURL)
   .then(() => {
     console.log("Database connected");
   })
   .catch((err) => {
     console.error(`Database is not connected. error message: ${err}`);
-  });
+  })
