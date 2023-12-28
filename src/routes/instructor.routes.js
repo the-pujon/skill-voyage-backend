333eb40a4ab49febb3c7124c24aa90f3f96588c5
Pujon Das Auvi <auvipujondas@gmail.com>
2023-12-28 18:55:45 +0600

    jwt verify added in instructors routes

--- src/routes/instructor.routes.js
+++ src/routes/instructor.routes.js
@@ -12,12 +12,14 @@ const {
   getSingleInstructor,
 } = require("../controllers/instructor.controller");
 
+const {verifyAdmin, verifyJWT} = require('../middlewares/auth')
+
 // Define routes for instructors
 router.get("", getInstructors); // GET all instructors with optional category filter
-router.post("/", addInstructor); // POST a new instructor
-router.get("/singleInstructor/", getSingleInstructor); // GET an instructor by email
-//router.get("/:id", getSingleInstructor); // GET an instructor by email
-router.put("/:email", updateInstructor); // UPDATE an instructor by email
-//router.delete("/:email", deleteInstructor); // DELETE an instructor by email
+router.post("/",verifyJWT, addInstructor); // POST a new instructor
+router.get("/:email", getSingleInstructor); // GET an instructor by email
+router.get("/:id", getSingleInstructor); // GET an instructor by email
+router.put("/:email",verifyJWT, updateInstructor); // UPDATE an instructor by email
+router.delete("/:email",verifyJWT, deleteInstructor); // DELETE an instructor by email
 
 module.exports = router;
