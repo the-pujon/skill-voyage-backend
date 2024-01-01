490730779851496a386f271092d4476e616ed45e
Pujon Das Auvi <auvipujondas@gmail.com>
2024-01-02 01:32:47 +0600

    stripe issue

--- app.js
+++ app.js
@@ -20,13 +20,27 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  if (req.originalUrl === "/webhook") {
-      next(); // Do nothing with the body because I need it in a raw state.
-  } else {
-      express.json()(req, res, next); // ONLY do express.json() if the received request is NOT a WebHook from Stripe.
-  }
-});
+
+app.use(
+  "/webhook",
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf.toString();
+    },
+  })
+);
+
+
+
+app.use(express.json());
+
+//app.use((req, res, next) => {
+//  if (req.originalUrl === "/webhook") {
+//      next(); // Do nothing with the body because I need it in a raw state.
+//  } else {
+//      express.json()(req, res, next); // ONLY do express.json() if the received request is NOT a WebHook from Stripe.
+//  }
+//});
 
 //app.use(require('body-parser').text({ type: "*/*" }));
 
