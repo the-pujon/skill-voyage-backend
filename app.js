36bebb2c702c9ff18c72e4b48b788eba99007bff
Pujon Das Auvi <auvipujondas@gmail.com>
2024-01-02 01:37:38 +0600

    stripe issue

--- app.js
+++ app.js
@@ -120,7 +120,24 @@ app.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (reques
       const event = stripe.webhooks.constructEvent(payloadString, header, secret);
       console.log('event', event)
 
-      //console.log(event.type)
+      console.log(event.type)
+      switch (event.type) {
+        case 'payment_intent.succeeded':
+          const paymentIntent = event.data.object;
+          console.log("pay", paymentIntent)
+          console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+          // Then define and call a method to handle the successful payment intent.
+          // handlePaymentIntentSucceeded(paymentIntent);
+          break;
+        case 'payment_method.attached':
+          const paymentMethod = event.data.object;
+          // Then define and call a method to handle the successful attachment of a PaymentMethod.
+          // handlePaymentMethodAttached(paymentMethod);
+          break;
+        default:
+          // Unexpected event type
+          console.log(`Unhandled event type ${event.type}.`);
+      }
 
 
 
