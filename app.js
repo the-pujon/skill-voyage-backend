704bbf26afbd2f823011b140ab48683d75509976
Pujon Das Auvi <auvipujondas@gmail.com>
2024-01-02 12:58:12 +0600

    payment updated

--- app.js
+++ app.js
@@ -89,15 +89,15 @@ app.post("/api/checkout", async (req, res) => {
     cancel_url: "http://localhost:5173",
   });
 
-  //const transaction = new paymentSchema({
-  //  sessionId: session.id,
-  //  email,
-  //  paymentStatus,
-  //  courses: products,
-  //});
-  //await transaction.save();
+  const transaction = new paymentSchema({
+    sessionId: session.id,
+    email,
+    paymentStatus,
+    courses: products,
+  });
+  await transaction.save();
 
-  //res.json({ sessionId: session.id });
+  res.json({ sessionId: session.id });
 
   res.json({ id: session.id });
 });
